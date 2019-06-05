9e49a45c5b2ce43e4b6e1ccaa2ab14d9977120ba
周彬 <44090409+ZhoubinXM@users.noreply.github.com>
2019-06-05 09:59:15 +0800

    Update testing.py

--- testing.py
+++ testing.py
@@ -9,31 +9,25 @@ def weight_variable(shape):
 	initial = tf.truncated_normal(shape, stddev=0.1)
 	return tf.Variable(initial)
 
-
 def bias_variable(shape):
 	initial = tf.constant(0.1, shape=shape)
 	return tf.Variable(initial)
 
-
 def conv2d(x, W):
 	return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
 
-def getBatchInput(inputs,start,batchSize) :
+def getBatchInput(inputs,start,batchSize):
 	first = start
 	start = start + batchSize
 	end = start
-	return inputs[first:end],start
+	return inputs[first:end], start
 
-outputFolder = 'testOutput'
-
+outputFolder = 'testOutput'    # 文档的建立
 if os.path.exists(outputFolder):
 	shutil.rmtree(outputFolder)
-
 os.mkdir(outputFolder)
 
-
 imageSize = [25,25,3]
-
 learningRate = 0.001
 lr_decay_rate = 0.9
 lr_decay_step = 2000
@@ -59,8 +53,7 @@ W_fc2 = weight_variable([100, 2])
 b_fc2 = bias_variable([2])
 
 y_out=tf.matmul(h_fc1, W_fc2) + b_fc2
-
-loss = tf.reduce_mean(tf.square(y_out),1)
+loss = tf.reduce_mean(tf.square(y_out),1)  # 计算Q值
 avg_loss = tf.reduce_mean(loss)
 
 global_step = tf.Variable(0, trainable=False)
@@ -78,43 +71,40 @@ saver.restore(sess, checkpointFile)
 
 
 testImages = 20
-limit = imageSize[0]*imageSize[1]
+limit = imageSize[0]*imageSize[1]       # limit=625
 point = np.genfromtxt('testPoints.txt')
 
-for i in range(testImages) :
+for i in range(testImages):
 	index = 0
+	inputs = []
 	outputImage = cv2.imread('testImages/image_'+str(i)+'_'+str(index)+'.png')
-	while not index == limit-1 :
-		print('index is %d'%index)
+	while not index == limit-1:
+		print('index is %d' % index)
 		inputImages = cv2.imread('testImages/image_'+str(i)+'_'+str(index)+'.png')
-		
-		inputs = []
 		inputs.append(inputImages)
 		inputs = np.array(inputs)
-		output = sess.run(y_out,feed_dict={x: inputs})
+		output = sess.run(y_out, feed_dict={x: inputs})
 		print(output[0])
-		if output[0][0] > output[0][1] :
-			if (index+1)%imageSize[0] == 0 :
+		if output[0][0] > output[0][1]:
+			if (index+1) % imageSize[0] == 0:
 				print('wrong grid entered')
 				break
 
-			index = index+1
+			index = index+1    # move right
 
-		else :
-			index = index+25
+		else:
+			index = index+25   # move bottom
 
-		if index>=limit or point[i*limit+index] == -100 :
+		if index>=limit or point[i*limit+index] == -100:
 			print('wrong grid entered')
 			break
-		else :
+		else:
 			outputImage[int(index/imageSize[0])][index%imageSize[0]][0] = 223
 			outputImage[int(index/imageSize[0])][index%imageSize[0]][1] = 27
 			outputImage[int(index/imageSize[0])][index%imageSize[0]][2] = 20
-			
 
-
-	if index == limit-1 :
-		cv2.imwrite(os.path.join(outputFolder,'image_'+str(i)+'.png'),outputImage)
+	if index == limit-1:
+		cv2.imwrite(os.path.join(outputFolder,'image_'+str(i)+'.png'), outputImage)
 
 
 
